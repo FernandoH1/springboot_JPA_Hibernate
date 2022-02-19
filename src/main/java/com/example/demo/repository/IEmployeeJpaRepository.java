ec0bc3fc2cc30750aa2ffda6d2ba894c08f58395
FernandoH1 <fernandohernandezclivio@gmail.com>
2022-02-19 18:40:55 -0300

    Segundo y Tercer video(Fix mas refactor de Packetes por errores) (Funcionando)

--- src/main/java/com/example/demo/repository/IEmployeeJpaRepository.java
+++ src/main/java/com/example/demo/repository/IEmployeeJpaRepository.java
@@ -1,12 +1,23 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
+import com.example.demo.model.Role;
 import com.example.demo.model.Employee;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface IEmployeeJpaRepository extends JpaRepository<Employee, Long> {
     // select fields from employee where employeeid='[param]'
     Employee findByEmployeeid(String employeeid);
+
+    List<Employee> findByFirstName(String firstName);
+
+    List<Employee> findByLastName(String lastName);
+
+    List<Employee> findByRole(Role role);
 
 }
